6d75294379a5062b3e6220821e8c465773038adc
Manish Kanadje <65487214+mkanadje@users.noreply.github.com>
2025-06-07 20:18:57 +0530

    fixed typo in start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-uvicorn app.main:app --host 0.0.0.0 --port 8080 &
+uvicorn app.main:app --host 0.0.0.0 --port 8501 &
 
-streamlit run ui/ui.py --server.port 8501 --server.address=0.0.0.0
+streamlit run ui/ui.py --server.port 8080 --server.address=0.0.0.0
